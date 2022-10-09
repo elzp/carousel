cd5604f4dd277f3ae4c63633a39f35b11029c783
elzp <86740659+elzp@users.noreply.github.com>
2022-10-09 10:06:32 +0200

    get access to logos & buttons

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // Import stylesheets
 import './style.css';
 
+const logos = document.querySelectorAll('div[data-id*="logo"]');
+const left = document.querySelector('button[data-id="left"]');
+const right = document.querySelector('button[data-id="right"]');
