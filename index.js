baa602c8c3ce3ef07cf10c300c2f911355ff580a
elzp <86740659+elzp@users.noreply.github.com>
2022-12-11 16:40:38 +0100

    Make smooth transition but ...
    with visible divs in hiding stage

--- index.js
+++ index.js
@@ -9,40 +9,84 @@ const status = Array.from(Array(logos.length)).map((it, index) => {
   return index === 0;
 });
 
-console.log(status);
-
-left.addEventListener('click', () => {
+right.addEventListener('click', () => {
   let idOfVisible = status.indexOf(true);
-  console.log(idOfVisible);
-  logos[idOfVisible].classList.add('hidden');
-  logos[idOfVisible].classList.remove('md:flex');
+
+  logos[idOfVisible].classList.remove('appear-to-right');
+  logos[idOfVisible].classList.add('disappear-to-right');
+  setTimeout(() => {
+    logos[idOfVisible].classList.add('hidden');
+    logos[idOfVisible].classList.remove('disappear-to-right');
+    logos[idOfVisible].classList.remove('md:flex');
+  }, 600);
+
   status[idOfVisible] = false;
 
   if (idOfVisible === 0) {
     logos[logos.length - 1].classList.remove('hidden');
+
+    logos[logos.length - 1].classList.add('appear-to-right');
     logos[logos.length - 1].classList.add('md:flex');
+
+    setTimeout(() => {
+      logos[logos.length - 1].classList.remove('appear-to-right');
+
+      logos[idOfVisible].classList.add('hidden');
+      logos[idOfVisible].classList.remove('disappear-to-right');
+    }, 1000);
     status[logos.length - 1] = true;
   } else {
     logos[idOfVisible - 1].classList.remove('hidden');
+
+    logos[idOfVisible - 1].classList.add('appear-to-right');
     logos[idOfVisible - 1].classList.add('md:flex');
+    setTimeout(() => {
+      logos[idOfVisible - 1].classList.remove('appear-to-right');
+      logos[idOfVisible].classList.add('hidden');
+      logos[idOfVisible].classList.remove('disappear-to-right');
+    }, 1000);
+
     status[idOfVisible - 1] = true;
   }
 });
 
-right.addEventListener('click', () => {
+left.addEventListener('click', () => {
   let idOfVisible = status.indexOf(true);
-  console.log(idOfVisible);
-  logos[idOfVisible].classList.add('hidden');
-  logos[idOfVisible].classList.remove('md:flex');
+
+  logos[idOfVisible].classList.remove('appear-to-left');
+  logos[idOfVisible].classList.add('disappear-to-left');
+  setTimeout(() => {
+    logos[idOfVisible].classList.add('hidden');
+    logos[idOfVisible].classList.remove('disappear-to-left');
+    logos[idOfVisible].classList.remove('md:flex');
+  }, 600);
+
   status[idOfVisible] = false;
 
   if (idOfVisible === logos.length - 1) {
     logos[0].classList.remove('hidden');
+
+    logos[0].classList.add('appear-to-left');
     logos[0].classList.add('md:flex');
+
+    setTimeout(() => {
+      logos[0].classList.remove('appear-to-left');
+
+      logos[idOfVisible].classList.add('hidden');
+      logos[idOfVisible].classList.remove('disappear-to-left');
+    }, 1000);
     status[0] = true;
   } else {
     logos[idOfVisible + 1].classList.remove('hidden');
+
+    logos[idOfVisible + 1].classList.add('appear-to-left');
     logos[idOfVisible + 1].classList.add('md:flex');
+    setTimeout(() => {
+      logos[idOfVisible + 1].classList.remove('appear-to-left');
+      logos[idOfVisible].classList.add('hidden');
+      logos[idOfVisible].classList.remove('disappear-to-left');
+    }, 1000);
+
     status[idOfVisible + 1] = true;
   }
 });
